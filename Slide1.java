c6a6e0ccbb1993426a6ccdd12e8d50078ccbbc9d
CaseyHaf <125267790+CaseyHaf@users.noreply.github.com>
2023-03-13 14:52:25 +0700

    Create Slide1.java

---
+++ Slide1.java
@@ -0,0 +1,27 @@
+public class Slide1 {
+    public static void main(String[] args) {
+        int a;
+        double d;
+        a = (1);
+        d = (1.0);
+
+        a=(46%9+9+4*4-2);
+        System.out.println("jawaban a1 " + a);
+
+        a=(45+43%5*(23*3%2));
+        System.out.println("jawaban a2 " + a);
+
+        a%=(3/a+3);
+        System.out.println("jawaban a3 " + a);
+
+        d+=(1.5*3+(++a));
+        System.out.println("jawaban d1 " + d);
+
+        System.out.println("  "); 
+
+        System.out.println("25/4 is " + 25/4);
+        System.out.println("25/4.0 is " + 25/4.0);
+        System.out.println("3*2/4 is " + 3*2/4);
+        System.out.println("3.0*2/4 is " + 3.0*2/4);
+    }
+}
