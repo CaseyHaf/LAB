4d6b9ead231aa05856b79b07fb3fdafd881e0213
CaseyHaf <125267790+CaseyHaf@users.noreply.github.com>
2023-03-13 15:02:23 +0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int i;
+        double k;
+        int j;
+        i = (0);
+        k = (100.0);
+        j = (i+1);
+        
+        System.out.println("j is " +j+ " and k is " +k);
+    }
+}
