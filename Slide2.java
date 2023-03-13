e4970df1ee0ade4105b44246a9bdf9c01a275ba5
CaseyHaf <125267790+CaseyHaf@users.noreply.github.com>
2023-03-13 15:30:08 +0700

    Create Slide2.java

---
+++ Slide2.java
@@ -0,0 +1,19 @@
+public class Slide2 {
+    public static void main(String args[]) {
+      char c = 'A';
+      int i = (int) c;
+      System.out.println("A = " +i);
+      
+      float f = 1000.34F;
+      int j = (int) f;
+      System.out.println("1000.34F = " +j);
+      
+      double d = 1000.34;
+      int k = (int) d;
+      System.out.println("1000.34 = " +k);
+      
+      int l = 97;
+      char n = (char) l;
+      System.out.println("97 = " +n);
+    }
+}
